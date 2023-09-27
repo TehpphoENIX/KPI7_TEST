8ada7c043e5e933c03d3a9f8766ba2726af49cbf
Teh_phoENIX <kirill.zaritskiy@gmail.com>
2023-09-27 10:16:30 +0300

    Added test file

---
+++ class.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+class Class
+{
+    Class()
+    {
+        std::cout << "I am constructor\n";
+    }
+
+    foo()
+    {
+        std::cout << "foo\n"
+    }
+}
